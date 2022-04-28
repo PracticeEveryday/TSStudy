2241a077329dd1fde81e46cfde19b08f65479efe
DongHyun Kim <developerforus@gmail.com>
2022-04-28 21:06:48 +0900

    todolist

---
+++ study2/src/TodoItem.ts
@@ -0,0 +1,35 @@
+// class TodoItem {
+//   public id: number; // private: 직접적인 접근 안 됨! , public :오픈!, protected
+//   public task: string;
+//   public complete: boolean;
+
+//    초기화 해줘야 함
+//   constructor(id: number, task: string, complete: boolean) {
+//     this.id = id;
+//     this.task = task;
+//     this.complete = complete;
+//   }
+// }
+
+// 타입 스크립트는 이렇게 생성자를 통해 초기화를 해야 되서 매번 두번 작성해야 되는 불상사가 나타남 쉽게 할려면?
+// 생성자에 직접적으로 접근 지정자를 준다!!
+// 이렇게 주면 프로퍼티로 바로 인식함
+
+class TodoItem {
+  constructor(
+    public id: number,
+    public task: string,
+    public complete: boolean
+  ) {
+    this.id = id;
+    this.task = task;
+    this.complete = complete;
+  }
+  printDetails(): void {
+    console.log(
+      `${this.id}\t${this.task}\t${this.complete ? "\t(complete)" : ""}`
+    );
+  }
+}
+
+export default TodoItem;
---
+++ study2/src/index.ts
@@ -0,0 +1,14 @@
+import TodoItem from "./TodoItem";
+
+// type 추론
+const data = [
+  { id: 1, task: "장보기", complete: true },
+  { id: 2, task: "TS 학습하기", complete: false },
+];
+
+console.log("My Todo List");
+
+for (let i = 0; i < data.length; i++) {
+  let todoItem = new TodoItem(data[i].id, data[i].task, data[i].complete);
+  todoItem.printDetails();
+}
