d65f241ee219081e6073b35bbe3dccf13a8601f9
DongHyun Kim <developerforus@gmail.com>
2022-04-29 00:23:35 +0900

    express + ts my version

--- study2_practice/build/src/app.js
+++ study2_practice/build/src/app.js
@@ -3,15 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/welcome", (req, res, next) => {
-    res.send("welcome!");
-});
-app.listen("1234", () => {
-    console.log(`
-  ################################################
-  🛡️  Server listening on port: 1234🛡️
-  ################################################
-`);
+exports.app = app;
+app.get("/", (req, res, next) => {
+    res.send("Hello World!");
 });
