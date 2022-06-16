bb0f8ccf9213c7fa7e964bc49606cf195d008cfc
DongHyun Kim <developerforus@gmail.com>
2022-06-16 17:28:18 +0900

    Type

---
+++ study3/root/class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+//var str = "hello";
+
+// TS 문자열 선언 문자열
+const str: string = "hello";
+
+// TS 숫자
+const num: number = 10;
+
+// TS 배열
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+const items: number[] = [1, 2, 3, 4]; // Array 배열 리터럴 가능
