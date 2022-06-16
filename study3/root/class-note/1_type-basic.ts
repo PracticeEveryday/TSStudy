a4d47d1e736b6022759fc7254b5b51d14670f0f8
DongHyun Kim <developerforus@gmail.com>
2022-06-16 17:38:14 +0900

    Function + Array Tuple

--- study3/root/class-note/1_type-basic.ts
+++ study3/root/class-note/1_type-basic.ts
@@ -11,3 +11,25 @@ const num: number = 10;
 const arr: Array<number> = [1, 2, 3];
 const heroes: Array<string> = ["Capt", "Thor", "Hulk"];
 const items: number[] = [1, 2, 3, 4]; // Array 배열 리터럴 가능
+
+// TS 튜플
+// 위치까지 정할 수 있음 배열의 인덱스에도 타입이 정해져 있음.
+// 모든 인덱스에 타입에 정해져 있는 배열
+let address: [string, number] = ["gamgnam", 100];
+
+// TS 객체를 정의하는 방식
+let obj: object = {};
+
+// let person: object = {
+//   name: "capt",
+//   age: 100,
+// };
+
+// 구체적으로 객체의 타입까지 지원 가능함
+let person: { name: string; age: number } = {
+  name: "capt",
+  age: 100,
+};
+
+// TS 진위값
+let show: boolean = false;
---
+++ study3/root/class-note/function.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// 여러개 넣어도 됨!! JavaScript는
+// 뒤에 3개는 반응을 안함!!
+sum(10, 20, 30, 40, 50);
