3e3224ddf1841b2b5e0161ce4f65d533f26a65af
DongHyun Kim <developerforus@gmail.com>
2022-07-09 08:48:56 +0900

    class

---
+++ study3/root/class-note/class.js
@@ -0,0 +1,26 @@
+// ES2015 (ES6 ) 최신 문법!
+
+// class 예약어 + 식별자 + {} => 인스턴스를 만들어 주는 게 역할
+class Person {
+  // 클래스 로직
+  constructor(name, age) {
+    console.log("생성 되었습니다.");
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const seho = new Person("세호", 30); // 생성 되었씁니다.
+
+console.log(seho);
+
+const admin = {};
+console.log(admin.__proto__ === seho.__proto__);
+console.log(admin.__proto__ === seho.__proto__.__proto__);
+console.log(admin.__proto__);
+console.log(seho.__proto__.__proto__);
+
+const lee = { name: "lee" };
+console.log(lee.__proto__);
+admin.__proto__ = seho;
+console.log(admin.name);
