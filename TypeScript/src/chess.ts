f035b263462c1246d7d7d3a73ec1a9a7262d2075
DongHyun Kim <developerforus@gmail.com>
2022-08-03 14:24:25 +0900

    Class

---
+++ TypeScript/src/chess.ts
@@ -0,0 +1,58 @@
+type Color = "Black" | "White";
+// x축을 파일이라고 하고
+type File = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
+// y축을 랭크라고 한다.
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+// 체스 게임
+class Game {}
+
+// 체스 말
+// abstract 키워드는 해당 클래스를 바로 인스턴스화 할수 없음을 의미할 뿐 필요한 매서드를 추상 클래스에서 자유롭게 구현할 수 있다.
+abstract class Piece {
+  protected position: Position;
+  constructor(
+    private readonly color: Color,
+    public file: File,
+    public rank: Rank
+  ) {
+    this.position = new Position(file, rank);
+  }
+
+  // public으로 다른 모든 코드에서 읽거나 쓸 수 있도록 허용한다.
+  public moveTo(position: Position) {
+    this.position = position;
+  }
+
+  abstract canMoveTo(position: Position): boolean;
+}
+
+// 체스 말의 좌표 집합
+class Position {
+  constructor(private file: File, private rank: Rank) {}
+  public distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
+}
+
+// 체스의 6가지 말
+class King extends Piece {
+  canMoveTo(position: Position): boolean {
+    let distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
+class Queen extends Piece {}
+class Bishop extends Piece {}
+class Knight extends Piece {}
+class Rook extends Piece {}
+class Pawn extends Piece {}
+
+/**
+ * public : 어디에서나 접근할 수 있다. 기본적으로 주어지는 접근 수준이다.
+ * protected : 이 클래스와 서브클래스 인스턴스에서만 접근할 수 있다.
+ * private : 이 클래스의 인스턴스에서만 접근 할 수 있다.
+ */
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,96 +1,106 @@
-console.log("Hello TypeScript!");
+// console.log("Hello TypeScript!");
 
-// let a = 1 + 2;
-// let b = a + 3;
-// let c = {
-//   apple: a,
-//   banana: b,
+// // let a = 1 + 2;
+// // let b = a + 3;
+// // let c = {
+// //   apple: a,
+// //   banana: b,
+// // };
+// // let d = c.apple * 4;
+
+// // any
+
+// let a: any = 666;
+// let b: any = ["danger"];
+// let c = a + b;
+
+// // unknown
+
+// let a: unknown = 30;
+// let b = a === 123;
+// let c = a + 10; // 객체의 타입이 unknown이다!
+// if (typeof a === "number") {
+//   let d = a + 10;
+//   console.log(d); // 40
+// }
+
+// // boolean
+
+// let a = true;
+// let b = false;
+// const c = true;
+// let d: boolean = true;
+// let e: true = true;
+// let f: true = false; // true 타입에 false 할당 할 수 없음.
+
+// // number
+
+// let a = 1234;
+// var b = Infinity * 0.1;
+// const c = 5678;
+// let d = a < b;
+// let e: number = 100;
+// let f: 26218 = 26218;
+// let g: 26218 = 10;
+
+// // bigint
+
+// let a: bigint = 1234n;
+// const b = 5678n;
+// var c = a + b;
+// let d = a < 1225;
+
+// // stirng
+
+// let a = "hello";
+// let b = "billy";
+// const c = "!";
+// let d = a + " " + b + c;
+// let f: "john" = "john";
+
+// // 객체
+// // object 내부에 b가 없다는 에러를 뱉음
+// // object를 쓰는 이유는 any보다 조금 좁은 타입으로 객체라고 말해 줄 뿐이다.
+// let a: object = {
+//   b: "x",
 // };
-// let d = c.apple * 4;
 
-// any
+// // 객체 리터럴 문법을 활용해보자
+// let a: { b: number } = {
+//   b: 12,
+// };
 
-let a: any = 666;
-let b: any = ["danger"];
-let c = a + b;
+// let c: { firstName: string; lastNmae: string } = {
+//   firstName: "john",
+//   lastNmae: "barrownman",
+// };
 
-// unknown
+// class Person {
+//   firstName;
+//   lastName;
+//   constructor(firstName: string, lastName: string) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
 
-let a: unknown = 30;
-let b = a === 123;
-let c = a + 10; // 객체의 타입이 unknown이다!
-if (typeof a === "number") {
-  let d = a + 10;
-  console.log(d); // 40
-}
+// const matt = new Person("matt", "smith");
 
-// boolean
+// console.log(matt);
 
-let a = true;
-let b = false;
-const c = true;
-let d: boolean = true;
-let e: true = true;
-let f: true = false; // true 타입에 false 할당 할 수 없음.
+// class Person {
+//   constructor(public firstNmae: string, public lastName: string) {}
+//   // public은 this.firstName = firstName을 단축한 것이다
+// }
 
-// number
+// let h = new Person("matt", "smith");
 
-let a = 1234;
-var b = Infinity * 0.1;
-const c = 5678;
-let d = a < b;
-let e: number = 100;
-let f: 26218 = 26218;
-let g: 26218 = 10;
-
-// bigint
-
-let a: bigint = 1234n;
-const b = 5678n;
-var c = a + b;
-let d = a < 1225;
-
-// stirng
-
-let a = "hello";
-let b = "billy";
-const c = "!";
-let d = a + " " + b + c;
-let f: "john" = "john";
-
-// 객체
-// object 내부에 b가 없다는 에러를 뱉음
-// object를 쓰는 이유는 any보다 조금 좁은 타입으로 객체라고 말해 줄 뿐이다.
-let a: object = {
-  b: "x",
-};
-
-// 객체 리터럴 문법을 활용해보자
-let a: { b: number } = {
-  b: 12,
-};
-
-let c: { firstName: string; lastNmae: string } = {
-  firstName: "john",
-  lastNmae: "barrownman",
-};
-
-class Person {
-  firstName;
-  lastName;
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+function map<T, U>(array: T[], f: (item: T) => U): U[] {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(f(array[i]));
   }
+  return result;
 }
 
-const matt = new Person("matt", "smith");
-
-console.log(matt);
-
-class Person {
-  constructor(public firstNmae: string, public lastName: string) {}
-  // public은 this.firstName = firstName을 단축한 것이다
-}
-
-let h = new Person("matt", "smith");
+map([1, 2, 3], (i) => i * 2);
